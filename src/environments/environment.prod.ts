4d6f16a9af6d2b46ca70a5c1ae1c78c566fdacc8
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2022-12-04 21:18:05 -0300

    feat: arquivo web.config adicionado para o IIS ler as rotas no deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlV1: 'http://localhost:5001/api/v1/',
-  imagensUrl: 'http://localhost:5001/'
+  apiUrlV1: 'https://localhost:5001/api/v1/',
+  imagensUrl: 'https://localhost:5001/'
 };
